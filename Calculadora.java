3c613eed914a5929ed32368b27ed70d3c501a172
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-02-18 21:45:48 -0300

    Adicionado metodo de soma juntamente com seu teste

--- Calculadora.java
+++ Calculadora.java
@@ -1,2 +1,5 @@
 public class Calculadora {
+    public double somar(double numA, double numB) {
+        return numA + numB;
+    }
 }
---
+++ TesteCalculadora.java
@@ -0,0 +1,9 @@
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println(calculadora.somar(1.7, 2.9));
+        System.out.println(calculadora.somar(7.25, 6.5));
+    }
+}
